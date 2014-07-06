38c33c566533f70807f8c65cd5072e4322d87399
Ben Tyler <benjamin.tyler@gmail.com>
2014-07-06 00:02:43 -0500

    First draft of Mojo::UserAgent::UnixSocket

---
+++ lib/DummySocket.pm
@@ -0,0 +1,11 @@
+package DummySocket;
+use parent 'IO::Socket::UNIX';
+
+# Mojo::UserAgent->_connected calls these: they're not important for a unix
+# socket, but the methods have to be there.
+sub sockhost { \1 };
+sub peerhost { \1 };
+sub sockport { -1 };
+sub peerport { -1 };
+
+1;
